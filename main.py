8d555313f3fb817fbe79b2bca4f5fd68e2310749
Auqherus <patrykromanowski@gmail.com>
2024-09-23 19:12:49 +0200

    C1: add some changes to main.py to 'get_shot' function

--- main.py
+++ main.py
@@ -9,15 +9,15 @@ def main():
 
             if self.health > 0:
                 self.health -= 1
-            raise ValueError(f"{self.name} is dead")
+            if self.health <= 0:
+                raise ValueError(f"{self.name} is dead")
 
         def shoot(self, target):  # working on it
 
-            archer = Archer(self.name, self.health, self.num_arrows)
-
-            if archer.num_arrows > 0:
-                archer.num_arrows -= 1
-                print(f"{archer.name} shot {archer.name}")
+            if self.num_arrows > 0:
+                self.num_arrows -= 1
+                # print(self.num_arrows)
+                print(f"{target.name} shot {self.name}")
                 self.get_shot()
             raise ValueError(f"{self.name} can't shoot")
 
@@ -28,6 +28,7 @@ def main():
 
         def print_status(self):
             print(f"{self.name} has {self.health} health and {self.num_arrows} arrows")
+
 
 main()
 
