15ce0797837a3aea63597cbbf8e1408b08b77967
Auqherus <patrykromanowski@gmail.com>
2024-09-23 18:20:14 +0200

    C: add some changes to main.py with 'raise ValueError'

--- main.py
+++ main.py
@@ -1,21 +1,34 @@
-class Archer:
-    def __init__(self, name, health, num_arrows):
-        self.name = name
-        self.health = health
-        self.num_arrows = num_arrows
+def main():
+    class Archer:
+        def __init__(self, name, health, num_arrows):  # it's done
+            self.name = name
+            self.health = health
+            self.num_arrows = num_arrows
 
-    def get_shot(self):
-        if self.health > 0:
-            self.health -= 1
-        raise ValueError(f"{self.name} is dead")
+        def get_shot(self):  # it's done
 
-    def shoot(self, target):
-        pass
+            if self.health > 0:
+                self.health -= 1
+            raise ValueError(f"{self.name} is dead")
 
-    # don't touch below this line
+        def shoot(self, target):  # working on it
 
-    def get_status(self):
-        return self.name, self.health, self.num_arrows
+            archer = Archer(self.name, self.health, self.num_arrows)
 
-    def print_status(self):
-        print(f"{self.name} has {self.health} health and {self.num_arrows} arrows")
+            if archer.num_arrows > 0:
+                archer.num_arrows -= 1
+                print(f"{archer.name} shot {archer.name}")
+                self.get_shot()
+            raise ValueError(f"{self.name} can't shoot")
+
+        # don't touch below this line
+
+        def get_status(self):
+            return self.name, self.health, self.num_arrows
+
+        def print_status(self):
+            print(f"{self.name} has {self.health} health and {self.num_arrows} arrows")
+
+main()
+
+
